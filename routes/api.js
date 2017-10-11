de2656340f03e12977248ff47c2fe75785c0a240
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-10-11 16:47:25 -0400

    changed scope

--- routes/api.js
+++ routes/api.js
@@ -3,39 +3,38 @@ const api = express.Router()
 const Sequelize = require("sequelize")
 const models = require("../models")
 
-/* GET users listing. */
-api.get("/", function(req, res, next) {
-  res.json({ hello: "world" })
-})
-
 ////Getting all of the words
-api.get("/allwords", (req, res) => {
+api.get("/", (req, res) => {
+  const flatten = arr => {
+    return arr.reduce(function(flat, toFlatten) {
+      return flat.concat(toFlatten.words)
+    }, [])
+  }
+  const sort = (a, b) => {
+    console.log({ a, b })
+    var nameA = a.toUpperCase() // ignore upper and lowercase
+    var nameB = b.toUpperCase() // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1
+    }
+    if (nameA > nameB) {
+      return 1
+    }
+
+    // names must be equal
+    return 0
+  }
   models.CategoriesAndWords
-    .findAll({ attributes: ["words", "video"] })
+    .findAll({ attributes: ["words", "videoId"] })
     .then(words => {
-      res.json(words)
+      res.json(flatten(words).sort(sort))
     })
     .catch(err => {
       console.log(err)
     })
 })
 
-api.get("allwords/:letter", (req, res) => {})
-
-//get just the word and the video ID- after commit, adding Video info
-api.get("/allwords/:id", (req, res) => {
-  const id = parseInt(req.params.id)
-  models.CategoriesAndWords
-    .findOne({ where: { id: id }, attributes: ["words", "videoId"] })
-    .then(words => {
-      models.Videos.findOne({ where: { id: words.videoId } }).then(video => {
-        res.json({ words, video })
-      })
-    })
-    .catch(err => {
-      console.log(err)
-    })
-})
+api.get("/letter/:letter", (req, res) => {})
 
 //Getting the list of categories
 api.get("/categories", (req, res) => {
@@ -68,6 +67,22 @@ api.get("/categories", (req, res) => {
     .findAll({ order: [["categories", "ASC"]], attributes: ["categories"] })
     .then(categories => {
       res.json(distinct(flatten(categories)).sort(sort))
+    })
+    .catch(err => {
+      res.json(err)
+    })
+})
+
+//get just the word and the video ID- after commit, adding Video info
+api.get("/word/:wordid", (req, res) => {
+  const id = parseInt(req.params.wordid)
+
+  models.CategoriesAndWords
+    .findOne({ where: { id: id }, attributes: ["words", "videoId"] })
+    .then(words => {
+      models.Videos.findOne({ where: { id: words.videoId } }).then(video => {
+        res.json({ words, video })
+      })
     })
     .catch(err => {
       console.log(err)
